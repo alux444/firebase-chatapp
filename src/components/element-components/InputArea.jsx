2c63e6cbdefe8393e1f75cb2ac0fbc72442b4cad
alux444 <aluux999@gmail.com>
2023-06-10 22:07:12 +1200

    message input bar

--- src/components/element-components/InputArea.jsx
+++ src/components/element-components/InputArea.jsx
@@ -1,9 +1,34 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, Button, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const InputArea = () => {
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    if (e.target.value.length < 100) {
+      setMessage(e.target.value);
+    }
+  };
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <Box sx={{ width: "100%", height: "20%", border: "1px solid red" }}></Box>
+    <Box sx={{ width: "100%", height: "20%", border: "1px solid red" }}>
+      <form onSubmit={handleSendMessage}>
+        <TextField
+          id="filled-basic"
+          label="Send Message Here"
+          variant="filled"
+          onChange={handleChange}
+          value={message}
+          sx={{ input: { color: "white" }, width: "80%" }}
+        />
+        <Button type="submit">Send Message</Button>
+        <Button>Ask Timmy</Button>
+      </form>
+    </Box>
   );
 };
 
--- src/components/element-components/MessagesArea.jsx
+++ src/components/element-components/MessagesArea.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 import React from "react";
 
 const MessagesArea = () => {
