e959b003615fc76fac20afff314df96593015033
alux444 <aluux999@gmail.com>
2023-07-04 22:22:38 +1200

    finished sign ups

--- src/components/element-components/SignupForm.jsx
+++ src/components/element-components/SignupForm.jsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
+import useFirebase from "../../utils/useFirebase";
 
 const SignupForm = ({ changePage }) => {
-  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirm, setConfirm] = useState("");
+  const [message, setMessage] = useState("");
 
-  const userHandle = (e) => {
-    if (e.target.value.length < 30) {
-      setUsername(e.target.value);
-    }
-  };
+  const { validateEmail, attemptLogin, createUser } = useFirebase();
 
   const emailHandle = (e) => {
     setEmail(e.target.value);
@@ -24,16 +21,33 @@ const SignupForm = ({ changePage }) => {
     setConfirm(e.target.value);
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    setMessage("");
+
+    if (password.length < 5) {
+      setMessage("Password is too short.");
+      return;
+    }
+
+    if (password !== confirm) {
+      setMessage("Passwords do not match.");
+      return;
+    }
+
+    const emailUsed = await validateEmail(email);
+
+    if (emailUsed) {
+      setMessage("Email is in use.");
+      return;
+    }
+
+    await createUser(email, false, password);
+    const loginSuccess = await attemptLogin(email, password);
   };
 
   return (
     <form className="flex flex-col gap-1 items-center" onSubmit={onSubmit}>
-      <div className="flex flex-col">
-        <small>Username</small>
-        <input type="text" value={username} onChange={userHandle} />
-      </div>
       <div className="flex flex-col">
         <small>Email</small>
         <input type="email" value={email} onChange={emailHandle} />
@@ -49,6 +63,7 @@ const SignupForm = ({ changePage }) => {
       <button type="submit" className="">
         Sign up
       </button>
+      <small>{message}</small>
       <button type="button" className="w-fit text-xs" onClick={changePage}>
         Login?
       </button>
--- src/utils/useFirebase.jsx
+++ src/utils/useFirebase.jsx
@@ -8,7 +8,8 @@ const useFirebase = () => {
   const { setUser } = useContext(UserContext);
   const usersRef = collection(db, "users");
 
-  const createUser = async (username, email, googleUser, password) => {
+  const createUser = async (email, googleUser, password) => {
+    const username = email.split("@")[0];
     const querySnapshot = await getDocs(
       query(usersRef, where("email", "==", email))
     );
@@ -42,13 +43,46 @@ const useFirebase = () => {
       localStorage.setItem("loggedInUser", JSON.stringify({ username, email }));
     } else {
       const username = email.split("@")[0];
-      createUser(username, email, true);
+      createUser(email, true);
       setUser({ loggedIn: true, username: username, email: email });
       localStorage.setItem("loggedInUser", JSON.stringify({ username, email }));
     }
   };
 
-  return { createUser, googleAttemptLogin };
+  const validateEmail = async (email) => {
+    const querySnapshot = await getDocs(
+      query(usersRef, where("email", "==", email))
+    );
+
+    if (!querySnapshot.empty) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const attemptLogin = async (email, password) => {
+    const querySnapshot = await getDocs(
+      query(usersRef, where("email", "==", email))
+    );
+
+    if (!querySnapshot.empty) {
+      const userDoc = querySnapshot.docs[0];
+      const userData = userDoc.data();
+      if (password == userData.password) {
+        const username = userData.username;
+        setUser({ loggedIn: true, username: username, email: email });
+        localStorage.setItem(
+          "loggedInUser",
+          JSON.stringify({ username, email })
+        );
+        return true;
+      }
+    }
+    return false;
+  };
+
+  return { createUser, googleAttemptLogin, validateEmail, attemptLogin };
 };
 
 export default useFirebase;
